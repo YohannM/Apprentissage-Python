189d24e7c23e189f8b70cf17cdd88f66cfbb90ec
MARMONIER YOHANN p1702174 <p1702174@univ-lyon1.fr>
2018-11-06 16:02:22 +0100

    arbres modif

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,2 @@
-plt
+
+        if noeud.fd == None:
--- testArbre.py
+++ testArbre.py
@@ -18,7 +18,7 @@ def placerValABR(noeud, val):
             placerValABR(noeud.fg, val)
     else:
         if noeud.fd == None:
-            noeud.fd == Noeud(val)
+            noeud.fd = Noeud(val)
         else:
             placerValABR(noeud.fd, val)
 
@@ -27,11 +27,11 @@ def creerABR():
         listeNoeuds = []
         for i in range(25):
             listeNoeuds.append(random.randint(0, 150))
+
         racine = Noeud(listeNoeuds[0])
-        print("Il y a", len(listeNoeuds, " valeurs")
-        for i in range(1, len(listeNoeuds)):
-            print("traitement de la val", i)
-            placerValABR(racine, listeNoeuds[i])
+
+        for i in range(len(listeNoeuds)-1):
+            placerValABR(racine, listeNoeuds[i+1])
         return racine
 
 def parcoursInfixe(racine):
@@ -40,34 +40,83 @@ def parcoursInfixe(racine):
         print(racine.val)
         parcoursInfixe(racine.fd)
 
+def parcoursPrefixe(racine):
+    if racine != None:
+        print(racine.val)
+        parcoursPrefixe(racine.fg)
+        parcoursPrefixe(racine.fd)
 
+def hauteur(racine):
+    if racine.fd == None and racine.fg == None:
+        return 1
+    elif racine.fd == None:
+        return hauteur(racine.fg) + 1
+    elif racine.fg == None:
+        return hauteur(racine.fd) + 1
+    else:
+        return max(hauteur(racine.fg), hauteur(racine.fd)) + 1
+
+def taille(racine):
+    if racine.fd == None and racine.fg == None:
+        return 1
+    elif racine.fd == None:
+        return taille(racine.fg) + 1
+    elif racine.fg == None:
+        return taille(racine.fd) + 1
+    else:
+        return taille(racine.fg) + taille(racine.fd) + 1
 
 def supprimer(ABR, val):
     feuilleASupp = ABR 
     while feuilleASupp.val != val:
-        feuilleASupp = feuilleASupp.fd if (feuilleASupp > val) else feuilleASupp.fg
-    fMin = feuilleASupp.fd
-    pereMin = feuilleASupp
-    if fMin != None:
-        while pereMin.fg.fg != None:
-            pereMin = pereMin.fg
+        if feuilleASupp.val > val:
+            feuilleASupp = feuilleASupp.fg
+        else:
+            feuilleASupp = feuilleASupp.fd
+    if feuilleASupp.fd != None:
+        pereMin = feuilleASupp.fd
+        if pereMin.fg != None:
             fMin = pereMin.fg
-        pereMin.fg = fMin.fd
-    feuilleASupp.val = fMin.val
+            while pereMin.fg.fg != None:
+                pereMin = pereMin.fg
+                fMin = pereMin.fg
+            pereMin.fg = fMin.fd
+        else :
+            fMin = pereMin
+            feuilleASupp.fd = pereMin.fd
+        feuilleASupp.val = fMin.val
+    elif feuilleASupp.fg != None:
+        pereMax = feuilleASupp.fg
+        if pereMax.fd != None :
+            fMax = pereMax.fd
+            while pereMax.fd.fd != None:
+                pereMax = pereMax.fd
+                fMax = pereMax.fd
+            pereMax.fd = fMax.fg
+        else :
+            fMax = pereMax
+            feuilleASupp = pereMax.fg
+        feuilleASupp.val = fMax.val   
+    else:
+        feuilleASupp = None
 
 
-def moha():
-    # function
+
 
 if __name__ == '__main__':
     arbre = creerABR()
-    parcoursInfixe(arbre)
-    supprimer(arbre, arbre.val)
+    print("hauteur : ", hauteur(arbre))
+    print("taille : ", taille(arbre))
+    parcoursPrefixe(arbre)
     print("\n\n")
+    supprimer(arbre, arbre.fg.val)
     parcoursInfixe(arbre)
-    moah()
+    print("taille : ", taille(arbre))
 
-
+    #while True:
+        #arbre = creerABR()
+       # supprimer(arbre, arbre.val)
+       # print("succes")
 
 
     
